72941068aeb63dbea1d540a82f78ec8de9c8b08a
Prabhat <anteshkumar114@gmail.com>
2024-06-26 23:29:26 +0530

    11 commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Quizee Quiz",
@@ -13,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))]   from-blue-200 via-blue-50 to-blue-100 min-h-screen  z-[-10] sticky top-0`}
+        className={` bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))]   from-blue-200 via-blue-50 to-blue-100 min-h-screen  z-[-10] sticky top-0`}
       >
         {children}
         <Toaster />
